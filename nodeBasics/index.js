deb67b57a573396cb50d29fc0c03c75f024529ec
Marina Skvortsova <marinaskvortsova380@gmail.com>
2026-06-26 00:11:51 -0500

    DZ 3.1 Intro to JS

---
+++ nodeBasics/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.greenBright("Hello World"));
+console.log(chalk.blueBright("My first Node.js script"));
+console.log(chalk.yellowBright("Homework 3.1"));
