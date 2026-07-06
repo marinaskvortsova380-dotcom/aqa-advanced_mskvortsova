b06f47127ef10f1424077052b013a6dfba712688
Marina Skvortsova <marinaskvortsova380@gmail.com>
2026-07-06 00:46:18 -0500

    Update homework 4.1 files with correct naming and latest changes on fix branch

--- jsBasics/Task2.js
+++ jsBasics/Task2.js
@@ -1,6 +1,8 @@
 const personName1 = "Bonnie";
 const personName2 = "Clyde";
-let greeting = `Hello, ${personName1} and ${personName2}`;
+let greeting1 = "Hello, " + personName1 + " and " + personName2;
 console.log(greeting);
 
-console.log(personName1 + " " + personName2);
+greeting = `Hello, ${personName1} and ${personName2}`;
+
+console.log(greeting);
--- jsBasics/Task3.js
+++ jsBasics/Task3.js
@@ -1,10 +1,5 @@
 let agePerson = 25;
 console.log(agePerson);
 
-if (agePerson <= 18)
-    console.log("Ти неповнолітній");
-
-else
-    console.log("Ти повнолітній");
-
-console.log(agePerson >= 18 ? "true" : "false");
+let isAdult = agePerson >= 18;
+console.log(isAdult);
