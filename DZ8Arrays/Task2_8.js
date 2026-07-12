e350cbc07fd122716be008349c54a9b83e530b0f
Marina Skvortsova <marinaskvortsova380@gmail.com>
2026-07-11 23:06:13 -0500

    DZ8Arrays

---
+++ DZ8Arrays/Task2_8.js
@@ -0,0 +1,5 @@
+const elements = [1, 2, 3, 4, 5];
+const doubledElements = elements.map(function (element) {
+    return element * 2
+})
+console.log("Подвоєні елементи: " + doubledElements)
---
+++ DZ8Arrays/Task6_8.js
@@ -0,0 +1,8 @@
+const numbersList = [1, 10, 14, 2, 4, 5, 43, 34];
+const sameNumbersList = numbersList.map(function (num) {
+    return num
+})
+sameNumbersList.sort(function (a, b) {
+    return a - b
+})
+console.log(sameNumbersList)
