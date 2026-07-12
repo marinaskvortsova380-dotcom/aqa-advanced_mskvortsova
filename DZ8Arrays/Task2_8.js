69f9f5b25f4c271758d2c838166ea7c1dc0fc240
Marina Skvortsova <marinaskvortsova380@gmail.com>
2026-07-12 00:25:24 -0500

    fix

--- DZ8Arrays/Task2_8.js
+++ DZ8Arrays/Task2_8.js
@@ -1,5 +1,5 @@
 const elements = [1, 2, 3, 4, 5];
-const doubledElements = elements.map(function (element) {
-    return element * 2
+const doubledElements = elements.map((number, index) => {
+    return number * index
 })
 console.log("Подвоєні елементи: " + doubledElements)
