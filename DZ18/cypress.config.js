6c5adc2a641962b006c8817329d9124a2f9ccfb3
Marina Skvortsova <marinaskvortsova380@gmail.com>
2026-08-18 08:47:36 -0500

    Update Cypress configuration, add specs and environment variables for DZ18

--- DZ18/cypress.config.js
+++ DZ18/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.feature",
+    specPattern: ["cypress/e2e/**/*.feature", "cypress/e2e/**/*.cy.js"],
     setupNodeEvents(on, config) {
       return require("./cypress/plugins/index.js")(on, config);
     },
@@ -13,9 +13,16 @@ module.exports = defineConfig({
     video: false,
     screenshotOnRunFailure: true,
     chromeWebSecurity: false,
+    numTestsKeptInMemory: 15,
     retries: {
       runMode: 2,
       openMode: 0,
     },
+    projectId: "yuf1hb",
+    env: {
+      apiServer: "https://swapi.info/api",
+      stageUser: "admin_test",
+      stagePassword: "SecretPassword123"
+    }
   },
 });
